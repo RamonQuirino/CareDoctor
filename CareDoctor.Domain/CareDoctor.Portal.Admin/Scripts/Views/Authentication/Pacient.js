a629c102d1e4dc6190cf7ca0604c3461c111a8e7
Ramon Quirino <ramon.ti@hotmail.com>
2015-09-07 13:24:11 -0300

    Corrigindo menus

--- CareDoctor.Domain/CareDoctor.Portal.Admin/Scripts/Views/Authentication/Pacient.js
+++
@@ -1,8 +0,0 @@
-﻿$scope.adicionaItem = function () {
-    $scope.itens.push({
-        produto: $scope.item.produto,
-        quantidade: $scope.item.quantidade,
-        comprado: false
-    });
-    $scope.item.produto = $scope.item.quantidade = '';
-};
