ec138790fd7909ef741b372f2fe95291083e24f5
Ramon Quirino <ramon.ti@hotmail.com>
2015-09-07 17:32:42 -0300

    Incluindo section

---
+++ CareDoctor.Domain/CareDoctor.Portal.Admin/Content/Scripts/People/index.js
@@ -0,0 +1,10 @@
+﻿//var CareDoctorApp = angular.module('CareDoctorApp', [])
+//CareDoctorApp.controller('PeopleController', function ($scope) {
+//    $scope.message = "Infrgistics";
+//});
+
+
+//angular.module("CareDoctorApp", [])
+//.controller('PeopleController', function ($scope) {
+//    $scope.message = { message: "hello" };
+//});
